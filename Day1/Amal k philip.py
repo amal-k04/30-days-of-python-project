c6dc200aa8df38fa48e7ada7887ce788792219d6
AMAL K PHILIP <amal17philip@gmail.com>
2024-11-13 07:25:01 +0530

    Add files via upload

---
+++ Day1/Amal k philip.py
@@ -0,0 +1,12 @@
+'''
+Author:Amal k philip
+Date:13-11-2024
+Description:Python variables are used to store data values.
+'''
+name = "Amal K Philip"
+age = 18
+height=6.0
+
+print("Name:", name,",",type(name))
+print("Age:", age,",",type(age))
+print("Height:", height,",",type(height))
