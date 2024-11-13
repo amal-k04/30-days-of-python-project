c40220bb4a8d8ec63f7ec1efce565d6d8df9ff0e
mariet-rose-george <marietrosegeorge2028@cs.sjcetpalai.ac.in>
2024-11-13 21:22:58 +0530

    Add files via upload

---
+++ Day1/mariet day 1.py
@@ -0,0 +1,11 @@
+'''
+AUTHOR:MARIET ROSE GEORGE
+DATE:13/11/24
+DESCRIPTION: Declare a variable and store a value in it, and then use the print() function to display that value and type.
+'''
+name="Mariet Rose George"
+age=18
+Height=5.8
+print("name is",name,"-",type(name))
+print("age is",age,"-",type(age))
+print("height is",Height,"-",type(Height))
