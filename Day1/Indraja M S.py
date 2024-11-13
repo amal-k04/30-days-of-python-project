7c725ad65cbdff3b5cd99d0ed3fee9828dc4a374
Indrajams2006 <indrajams2028@cs.sjcetpalai.ac.in>
2024-11-13 23:00:53 +0530

    Update Day1

---
+++ Day1/Indraja M S.py
@@ -0,0 +1,11 @@
+'''
+Author:Indraja M S
+Date:13-11-24
+Description:Python, variables are used to store data values.
+'''
+name="Indraja M S"
+age=18
+height=5.5
+print("Name:",name)
+print("Age:",age)
+print("Height:",height)
