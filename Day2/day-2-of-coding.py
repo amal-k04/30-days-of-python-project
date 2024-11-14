43c2e4864751fda8495993465a78aaef60d9b9c0
CYRIL2313 <cyrilaccq@gmail.com>
2024-11-14 22:02:00 +0530

    Add files via upload

---
+++ Day2/day-2-of-coding.py
@@ -0,0 +1,16 @@
+Author:CYRIL JACOB THOMAS
+Date:14-11-2024
+Description:basic mathematical operations such as addition, subtraction, multiplication, and division using operators. The result is displayed on screen.
+
+
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+addition=(num1+num2)
+print("ON ADDING WE GET",addition)
+subtraction=(num1-num2)
+print("ON SUBSTRACTING WE GET",subtraction)
+multiplication=(num1*num2)
+print("ON MULTIPLING WE GET",multiplication) 
+division=(num1/num2)
+print("ON DIVIDING WE GET",division)
+
