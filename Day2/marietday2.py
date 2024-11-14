4465e68b7ced22b4fd0c29e9c1e45afd936f8219
mariet-rose-george <marietrosegeorge2028@cs.sjcetpalai.ac.in>
2024-11-14 22:12:30 +0530

    Add files via upload

---
+++ Day2/marietday2.py
@@ -0,0 +1,12 @@
+'''
+AUTHOR:MARIET ROSE GEORGE
+DATE:13/11/24
+DESCRIPTION: Basic arithmetic operators
+'''
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+print("sum of two numbers",num1,"and",num2,"is",num1+num2)
+print("difference of",num1,"and",num2,"is",num1-num2)
+print("product of",num1,"and",num2,"is",num1*num2)
+print("quotient of",num1,"and",num2,"is",num1/num2)num1,"and",num2,"is",num1*num2)
+print("quotient of",num1,"and",num2,"is",num1/num2)
