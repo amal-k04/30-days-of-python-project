e35bcdc3f5b684daf1e88ab41301c5838e5fda38
Alwin-sabu-2006 <alwinsabu2028@cs.sjcetpalai.ac.in>
2024-11-14 19:26:55 +0530

    Add files via upload

---
+++ Day2/Arithemetic operators in python-Alwin Sabu.py
@@ -0,0 +1,15 @@
+'''
+Author=Alwin Sabu
+Description=A python programe that uses arithemetic operators"
+Date=14/11/24
+'''
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+add=num1+num2
+print("The sum" ,num1,"and",num2, "is:",add)
+sub=num1-num2
+print("The difference of",num1,"and",num2,"is:",sub)
+multiply=num1*num2
+print("The product of ",num1,"and",num2,"is:",multiply)
+div=num2/num1
+print("The quotient of",num2,"and",num1,"is:",div)
