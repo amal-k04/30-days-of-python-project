f652a23c6ae8e2ab91737de97675530ab5afffcb
Indrajams2006 <indrajams2028@cs.sjcetpalai.ac.in>
2024-11-14 21:22:13 +0530

    Update Day2

---
+++ Day2/Indraja M S.py
@@ -0,0 +1,11 @@
+'''
+Author:Indraja M S
+Date:14-11-24
+Description:A Python programe to perform basic arithmetic operations.
+'''
+num1=int(input("Enter the first  number:"))
+num2=int(input("Enter the second number:"))
+print("sum of number1 and number2 is",num1+num2)
+print("Difference of number2 from number1 is",num1-num2)
+print("Product of number 1 and number2 is",num1*num2)
+print("Quotient of number1 by number2 is",num1/num2)
