bfc31bd66fe36b8a08f6743f22c4aa7b6945762f
AMAL K PHILIP <amal17philip@gmail.com>
2024-11-14 06:33:15 +0530

    Add files via upload

---
+++ Day2/Amal k philip.py
@@ -0,0 +1,11 @@
+'''
+Author:Amal k philip
+Date:14-11-2024
+Description:basic mathematical operations such as addition, subtraction, multiplication, and division using operators. The result is displayed on screen.
+'''
+num1=int(input("enter the number:"))
+num2=int(input("enter the number:"))
+print("Sum of number1 and number2 is",num1+num2)
+print("Difference of number2 from number1 is",num1-num2)
+print("Product of number1 and number2 is",num1*num2)
+print("Quotient of number1 by number2 is",num1/num2)
