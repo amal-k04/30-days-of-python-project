30bca2aaacad4fe81ca2b598516df69a28f66ae7
mariet-rose-george <marietrosegeorge2028@cs.sjcetpalai.ac.in>
2024-11-15 22:22:10 +0530

    Add files via upload

---
+++ Day3/mariet day3.py
@@ -0,0 +1,12 @@
+'''
+author:Mariet Rose George
+date:15/11/24
+Description:Writing a program that takes an integer input from the user and prints whether the number is positive, negative, or zero.
+'''
+num=int(input("enter a number:"))
+if num>0:
+    print("the number is positive")
+elif num<0:
+    print("the number is negative")
+else:
+    print("the number wil be zero")      
