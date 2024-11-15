9cca2e92f602370f01563471fc58f11bd403dbe4
AMAL K PHILIP <amal17philip@gmail.com>
2024-11-15 21:30:03 +0530

    Amal k philip.py

---
+++ Day3/Amal k philip.py
@@ -0,0 +1,12 @@
+''' 
+Author:Amal K Philip
+Date:15-11-2024
+Description:program that takes an integer input from the user and prints whether the number is positive, negative, or zero.'''
+
+num = int(input("Enter a number: "))
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
