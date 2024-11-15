200ddef2281251c086eb653c303e1feb9b80ff8f
Alwin-sabu-2006 <alwinsabu2028@cs.sjcetpalai.ac.in>
2024-11-15 22:47:10 +0530

    Add files via upload

---
+++ Day3/POSITIVE_NEGATIVE.PGM-ALWIN SABU.py
@@ -0,0 +1,11 @@
+'''
+AUTHO=ALWIN SABU
+DESCRIPTION=A PRGM THAT IDENTIFIES WHETHER THE ENTERED NUMBER IS POSITIVE,NEGATIVE,ZERO
+'''
+num=int(input("Enter a Number:"))
+if num<0:
+    print("The number is negative")
+elif num>0:
+    print("The number is positive")
+else:
+    print("The number is Zero")
