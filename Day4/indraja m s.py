df9f096c2c5bcb09dac3747fed88f55bad13087b
Indrajams2006 <indrajams2028@cs.sjcetpalai.ac.in>
2024-11-16 20:15:07 +0530

    Update Day4

---
+++ Day4/indraja m s.py
@@ -0,0 +1,12 @@
+''' 
+Author:Indraja M S
+Date:16-11-2024
+Description:
+a program that takes an integer as input and returns True if the number is even, 
+otherwise returns False.
+'''
+num=int(input("Enter a number:"))
+if(num%2==0):
+   print("The number",num,"is Divisible by 2,its EVEN")
+else:
+   print("is not divisible by 2,its ODD"
