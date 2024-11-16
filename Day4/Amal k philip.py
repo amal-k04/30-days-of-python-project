2ec9bfcc363e45cf4e63369a4b4ea87047024d9f
AMAL K PHILIP <amal17philip@gmail.com>
2024-11-16 08:31:44 +0530

    Add files via upload

---
+++ Day4/Amal k philip.py
@@ -0,0 +1,10 @@
+'''
+Author:Amal k philip
+Date:14-11-2024
+Description: program that takes an integer as input and returns True if the number is even, otherwise print odd.
+'''
+num=int(input("enter the number:"))
+if num%2==0:
+    print("The number",num,"is Divisible by 2, it's EVEN.")
+else:
+    print("The number",num,"is not Divisible by 2,it's ODD.")
