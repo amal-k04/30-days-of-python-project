239a39af88c704c515f6f5bfd4a807dca18a29c1
Alwin-sabu-2006 <alwinsabu2028@cs.sjcetpalai.ac.in>
2024-11-16 19:08:51 +0530

    Add files via upload

---
+++ Day4/Day 4 of coding-Alwin sabu.py
@@ -0,0 +1,12 @@
+'''
+AUTHOR= ALWIN SABU
+DESCRIPTION= program that takes an integer as input and returns True
+if the number is even, otherwise print odd.
+DATE=16/11/24
+'''
+num=int(input("Enter the number that you want to check:"))
+if num%2==0:
+    print("The number",num,"is divisible by 2 it is even")
+else:
+    print("The number",num,"is  not divisible by 2 it is odd")
+    
