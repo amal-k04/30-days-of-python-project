709c96957a5925f5ca894e0c8165f98f0603ba84
CYRIL2313 <cyrilaccq@gmail.com>
2024-11-16 17:01:26 +0530

    day-3-of-coding-cyril.py

--- Day3/day-3-of-coding-cyril.py
+++ Day3/day-3-of-coding-cyril.py
@@ -1,3 +1,7 @@
+Author:cyril
+Date:15-11-2024
+Description:program that takes an integer input from the user and prints whether the number is positive, negative, or zero.
+
 NUMBER=int(input("Enter the numbear"))
 if NUMBER>0:
     print("Numbear is positive")
