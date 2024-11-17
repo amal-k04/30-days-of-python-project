a28393596b732beaf45200893dcdb9b69e55a9a4
CYRIL2313 <cyrilaccq@gmail.com>
2024-11-17 14:01:41 +0530

    day-4-of-coding-cyril.py

--- Day4/day-4-of-coding-cyril.py
+++ Day4/day-4-of-coding-cyril.py
@@ -1,3 +1,7 @@
+Author:cyril
+Date:14-11-2024
+Description: program that takes an integer as input and returns True if the number is even, otherwise print odd.
+
 num=int(input("Enter the number"))
 while True:
    if num%2 == 0:
