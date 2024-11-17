415c0da2614d89056885c6d9b2e7c6081795b38b
Indrajams2006 <indrajams2028@cs.sjcetpalai.ac.in>
2024-11-17 20:17:06 +0530

    Update Day3

---
+++ Day3/indraja.py
@@ -0,0 +1,13 @@
+'''
+Authour:Indraja M S
+Date:15-11-2024
+Description:a program that takes an integer input from the user and prints 
+whether the number is positive, negative, or zero.
+'''
+num=int(input("Enter a number:"))
+if num>0:
+    print("The number is postive.")
+elif num<0:
+    print("The number is negative.")
+else:
+   print("The number is zero.")
