e2595d4dc5cd97a44990ef65b0faa882cb741150
mariet-rose-george <marietrosegeorge2028@cs.sjcetpalai.ac.in>
2024-11-17 09:22:21 +0530

    Add files via upload

---
+++ Day4/marietday4.py
@@ -0,0 +1,10 @@
+'''
+author:Mariet Rose George
+date:15/11/24
+Description:Write a program that takes an integer as input and returns True if the number is even, otherwise returns False.
+'''
+num=int(input("Enter a number:"))
+if(num%2==0):
+   print("The number",num,"is Divisible by 2 and it is even")
+else:
+   print("the number is not divisible by 2and it is ODD")
