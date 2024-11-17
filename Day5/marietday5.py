9e36fa568d1b5348bdbab6ca70e1ffa64ab35be3
mariet-rose-george <marietrosegeorge2028@cs.sjcetpalai.ac.in>
2024-11-17 22:00:28 +0530

    Add files via upload

---
+++ Day5/marietday5.py
@@ -0,0 +1,10 @@
+'''
+author:Mariet Rose George
+date:15/11/24
+Description:Multiplication table
+'''
+num=int(input("Enter a number:"))
+i=1
+while i<=10:
+    print(f"{num}×{i}={num*i}")
+    i+=1    
