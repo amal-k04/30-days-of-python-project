ce3a47787d8ac97b27a30fb1c38454442e22e8cc
CYRIL2313 <cyrilaccq@gmail.com>
2024-11-17 19:20:25 +0530

    day-5-of-coding-cyril.py

--- Day5/day-5-of-coding-cyril.py
+++ Day5/day-5-of-coding-cyril.py
@@ -1,3 +1,7 @@
+AUTHOR = CYRIL
+DESCRIPTION=a program to print the multiplication table of a given number using a while loop upto 10 times.The result can display using print() function.
+DATE=17/11/24
+
 num=int(input("ENter the number"))
 i=1
 while i<=10:
