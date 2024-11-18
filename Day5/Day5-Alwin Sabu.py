71f2f67a2baaafa67fd0dac7840eaae79a82fd61
Alwin-sabu-2006 <alwinsabu2028@cs.sjcetpalai.ac.in>
2024-11-18 19:18:05 +0530

    update  Day5-Alwin Sabu.py

--- Day5/Day5-Alwin Sabu.py
+++ Day5/Day5-Alwin Sabu.py
@@ -4,6 +4,8 @@ DESCRIPTION=a program to print the multiplication table of a given number using 
 DATE=17/11/24
 '''
 num=int(input("Enter number:"))
-for i  in range (0,11):
+i=0
+while i<11:
     num2=num*i
     print(num,"*",i,"=",num2)
+    i=i+1
