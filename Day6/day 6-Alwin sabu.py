2275e25edc78ed2070381896767216933af8ae6c
Alwin-sabu-2006 <alwinsabu2028@cs.sjcetpalai.ac.in>
2024-11-18 19:29:51 +0530

    Add files via upload

---
+++ Day6/day 6-Alwin sabu.py
@@ -0,0 +1,11 @@
+'''
+Author=Alwin sabu
+description=a program that takes a string and reverses it using a for loop.
+Date=18/11/24
+'''
+initial_string=input("Enter the string:")
+reverse=" "
+for i in initial_string:
+    reverse=i+reverse
+print("Reverse string=",reverse)
+    
