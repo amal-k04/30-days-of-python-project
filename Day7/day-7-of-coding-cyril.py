a9d8237299108a3abedaa79fbab9fd88e1896260
CYRIL2313 <cyrilaccq@gmail.com>
2024-11-19 20:49:29 +0530

    day-7-of-coding-cyril.py

--- Day7/day-7-of-coding-cyril.py
+++ Day7/day-7-of-coding-cyril.py
@@ -1,3 +1,9 @@
+#Author=cyril
+#DESCRIPTION=a program to count the number of digits in a given
+#integer using a while loop.And Display the sum of digits.
+#DATE=19/11/24
+
+
 num=input("Enter the number")
 length=0
 i=0
