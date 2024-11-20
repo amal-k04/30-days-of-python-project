ee3502f027ac857cc6235af0ce64eaf0ef482cf4
AMAL K PHILIP <amal17philip@gmail.com>
2024-11-20 21:06:54 +0530

    Add files via upload

---
+++ Day8/Amal k philip.py
@@ -0,0 +1,11 @@
+"""
+Author:AMAL K PHILIP
+Date:20-11-2024
+Description:Python program to calculate the factorial of a given number.
+"""
+
+number=int(input("Enter a8 Positive number:"))
+factorial=1
+for i in range(1,number+1):
+    factorial=factorial*i
+print(f"The factorial of {number} is {factorial}")
