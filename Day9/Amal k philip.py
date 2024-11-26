5d0129b22cc6536c565b468ba7f1c6696a25d6f3
AMAL K PHILIP <amal17philip@gmail.com>
2024-11-26 22:34:55 +0530

    Amal k philip.py

---
+++ Day9/Amal k philip.py
@@ -0,0 +1,12 @@
+'''
+Author:Amal k philip
+Date:21-11-2024
+Description:Python program to generate the Fibonacci sequence up to a specified number of terms.
+'''
+
+limit=int(input("Enter the number of terms in Fibonacci Series"))
+a=0
+b=1
+for times in range(0,limit):
+    print(a,end=" ")
+    a,b=b,a+b
